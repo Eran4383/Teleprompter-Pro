352a068cc0d5a2a32b1941b2045d237c91c7cf95
Eran4383 <129061889+Eran4383@users.noreply.github.com>
2026-01-28 17:34:23 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/teleprompter-pro/',
+  base: './',  // <--- זה התיקון הקריטי! (במקום השם המלא)
   build: {
     outDir: 'dist',
     emptyOutDir: true,
